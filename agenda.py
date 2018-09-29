e40b4db99001d43485c08ea6a1e72ffd8460b0e5
Eduardo Vieira <eduardovieira2@gmail.com>
2018-09-28 23:12:25 -0300

    mudando o arquivo agenda.py

--- agenda.py
+++ agenda.py
@@ -1,19 +1,7 @@
 #Agenda Telefonica
+import sys
 import funcoes
 
 funcoes.bemvindo()
 
-#Opcoes do Usuario
-opcao = int(raw_input())
-print("Selecionou a ", opcao)
-
-
-#Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-else:
-	funcoes.falha()
-
-
+#print("Selecionou a ", opcao)
